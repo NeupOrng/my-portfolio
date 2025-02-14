571dc8326457af04dc62d8f1d6685c5472de3620
orng.neu <orng.neu@ascendcorp.com>
2025-02-14 16:10:14 +0700

    install dependency

--- app.vue
+++ app.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
+  <div class="w-full min-h-screen">
+    <div class="absolute top-0 right-0 flex justify-end items-center p-3">
+      <button class="primary-button" @click="changeColor">{{ changeColorButtonText }}</button>
+    </div>
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+  const colorMode = useColorMode();
+  colorMode.preference = 'light';;
+
+
+  // methods
+  const changeColor = () => {
+    colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light';
+  }
+
+  // computed
+  const changeColorButtonText = computed(() => colorMode.preference === 'light' ? 'dark' : 'light')
+</script>
+
+
 <style>
 .page-enter-active,
 .page-leave-active {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,52 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: "./",
+
   app: {
     pageTransition: {
       name: 'page',
       mode: 'out-in'
     }
   },
+
+  tailwindcss: {
+    cssPath: ['~/assets/css/main.css', { injectPosition: 'last'}],
+    config: {
+      content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+      ],
+      theme: {
+        extend: {
+        },
+      },
+      plugins: [],
+    }
+  },
+
   runtimeConfig: {
     public: {
       baseURL: process.env.BASE_URL
     }
   },
+
   modules: [
-    '@nuxt/ui',
+    '@nuxtjs/color-mode',
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
   ],
 
-  tailwindcss: {
-    exposeConfig: true,
-    viewer: true
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
 
   devtools: { enabled: true },
-  compatibilityDate: '2025-02-13'
+  compatibilityDate: "2025-02-14",
 })
