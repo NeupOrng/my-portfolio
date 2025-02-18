13a99896a05d330aa8ab002b7c91c955040357af
orng.neu <orng.neu@ascendcorp.com>
2025-02-18 11:11:11 +0700

    add blog page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <div class="fixed top-0 left-0 w-full background-primary py-3 z-10">
-      <section class="relative flex justify-end items-center mx-auto max-w-[1440px] px-2 z-10">
+    <div class="fixed top-0 left-0 w-full background-primary py-3 z-10 flex justify-between">
+      <section class="w-full px-2 font-mono">
+        <h1 class="font-bold text-2xl"><span class="text-[#C8F31D]">O</span>RNG</h1>
+      </section>
+      <section class="relative gap-2 w-full flex justify-end items-center mx-auto max-w-[1440px] px-2 z-10">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/blog">Blog</NuxtLink>
         <button class="secondary-button" @click="theme.changeMode">
           <div class="relative size-[18px] overflow-hidden">
             <span :class="theme.theme === 'dark' ? 'animate-rise' : 'animate-shine'" :hidden="theme.theme === 'dark'"
@@ -45,5 +50,30 @@ const theme = useThemeStore();
 .animate-rise {
   animation: rise 0.3s ease-in-out;
   opacity: 1;
+}
+@keyframes linkActive {
+  0% {
+    width: 0;
+    opacity: 0;;
+  }
+
+  100% {
+    width: 100%;
+    opacity: 1;
+  }
+}
+.router-link-active {
+  color: var(--ui-secondary-button-background-color);
+}
+.router-link-active::after {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 1px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: var(--ui-secondary-button-background-color);
+  transition: 0.5s;
+  animation: linkActive 0.5s ease-in-out;
 }
 </style>
